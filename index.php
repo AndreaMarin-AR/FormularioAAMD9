e1c6872c886108f53f717c31c94c6e83d493dc14
AndreaMarin-AR <andreamarin751@gmail.com>
2020-03-30 13:05:31 -0430

    mi primer commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio</title>
+</head>
+<body>
+<strong>Bienvenidos</strong>
+
+
+<ul>
+	<li><a href="formulario.html">Formulario</li>
+	<li><a href="#">Módulo 2</li>
+	<li><a href="#">Módulo 3</li>
+</ul>
+</body>
+</html>
